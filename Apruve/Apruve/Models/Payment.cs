3f9b7b1b790408e5a238770819763ce6251542f9
Matthew Ostman <mrostman@syr.edu>
2014-07-01 09:52:49 -0500

    Added fields to Payment

--- Apruve/Apruve/Models/Payment.cs
+++ Apruve/Apruve/Models/Payment.cs
@@ -8,7 +8,7 @@ namespace Apruve.Models
 {
     public class Payment
     {
-        
+
         //Set by Apruve - do not specify these on POST
         public string id { get; set; }
         public string payment_request_id { get; set; }
@@ -16,13 +16,25 @@ namespace Apruve.Models
         public string api_url { get; set; }
         public string view_url { get; set; }
         public string created_at { get; set; }
+        public string updated_at { get; set; }
 
         //Required on POST
         public int amount_cents { get; set; }
-        
+
         //Optional Params
         public string currency { get; set; }
         public string merchantNotes { get; set; }
+        public List<PaymentItem> payment_items { get; set; }
+
+        public static Payment get(string paymentRequestId, string paymentId)
+        {
+            var apruveClient = ApruveClient.getInstance();
+            string uri = "/api/v3/payment_requests/" + paymentRequestId + "/payments/" + paymentId;
+            var apruveResponse = apruveClient.get<Payment>(uri);
+
+            // Return the response object, which should be a PaymentRequest object
+            return apruveResponse;
+        }
 
         public static Payment create(Payment payment)
         {
@@ -34,11 +46,12 @@ namespace Apruve.Models
             // Return the response object, which should be a Payment object
             return apruveResponse;
         }
+
         // Paths for API Calls
         public void setApiUrl(string apiUrl)
         {
             this.api_url = apiUrl;
         }
-        
+
     }
 }
